1e9d52d66bb818c233dd534bbae3c220266a3c9a
SanifKandel <sanifkandel12345@gmail.com>
2022-05-19 15:52:28 +0545

    Forget Pass

--- Sahayog/settings.py
+++ Sahayog/settings.py
@@ -83,9 +83,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sahayog',
         'USER': 'root',
-        'PASSWORD': '123456abc',
+        'PASSWORD': '#J@v@978',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': '3306'
     }
 }
 
@@ -136,3 +136,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SMTP Configuration
+LOGIN_URL ='/login/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sahayog@gmail.com'
+EMAIL_HOST_PASSWORD = 'sahayog123'
