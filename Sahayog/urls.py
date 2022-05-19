22f08206bdcab903b8bf6582bc52704e4c903903
utsavprz <utsavprajapati17@gmail.com>
2022-05-19 14:32:20 +0545

    Frontend for registration and login

--- Sahayog/urls.py
+++ Sahayog/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('accounts/',include('accounts.urls')),
+    path('accounts/',include('accounts.urls')),
     path('',include('home.urls')),
     # path('donate/',include('donate.urls')),
     # path("mydonation/", include('mydonation.urls')),
---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls.conf import include
+from django.urls import path
+
+from . import views
+
+app_name='accounts'
+
+urlpatterns = [
+    path("login", views.loginPage, name="loginPage"),
+    path("register", views.registerPage, name="registerPage"),
+]
