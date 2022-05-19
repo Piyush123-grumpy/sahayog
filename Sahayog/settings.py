d185436782f41539cb0ddf92ef130467ec0d89aa
SanifKandel <sanifkandel12345@gmail.com>
2022-05-19 23:00:06 +0545

    Email Details

--- Sahayog/settings.py
+++ Sahayog/settings.py
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sahayog@gmail.com'
-EMAIL_HOST_PASSWORD = 'sahayog123'
+EMAIL_HOST_USER = 'sahayog577@gmail.com'
+EMAIL_HOST_PASSWORD = 'S@hayog123'
