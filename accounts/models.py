592bf966504fe3d01bb32d04d351ca9b01e0a30b
Piyush <piyush.ratna.64@gmail.com>
2022-05-19 12:29:58 +0545

    init commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
 
 # Create your models here.
+class User(AbstractUser):
+    phone_number=models.CharField(verbose_name="Phone Number",max_length=30)
+    product_image=models.ImageField(upload_to='shop/images',default='', null=True, blank=True)
