e68dc862dbc345b2253d9082b7f7be2e2b0644a4
Piyush <piyush.ratna.64@gmail.com>
2022-05-20 10:11:41 +0545

    User info create

--- Sahayog/settings.py
+++ Sahayog/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sahayog',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '123456abc',
         'HOST': 'localhost',
         'PORT': '3306',
     }
